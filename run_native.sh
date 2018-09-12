27d7df39283e446054d72578e6fa19d993d95d88
Marcus Böhm <wif.mboehm@gmx.de>
2018-09-12 18:23:49 +0200

    don't run a server

--- run_native.sh
+++ run_native.sh
@@ -7,6 +7,6 @@ cbindgen -o hello.h -l C src/main/rust/hello.rs
 echo ">>>>>>>>>>>> Compiling the scala sources"
 sbt package
 echo ">>>>>>>>>>>> Generating the native image"
-native-image -H:Name=hello --verbose -cp target/scala-2.12/hello-scala_2.12-0.1.0-SNAPSHOT.jar:/Users/marcusboehm/.ivy2/cache/net.java.dev.jna/jna/jars/jna-4.5.2.jar:/Users/marcusboehm/.sbt/boot/scala-2.12.6/lib/scala-library.jar -H:Class=example.Hello -H:+JNI -H:IncludeResources=libhello.dylib -Djava.library.path=. -H:CLibraryPath=. -Djna.debug_load=true -Djna.library.path=/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jnalib
+native-image -H:Name=hello --no-server --verbose -cp target/scala-2.12/hello-scala_2.12-0.1.0-SNAPSHOT.jar:/Users/marcusboehm/.ivy2/cache/net.java.dev.jna/jna/jars/jna-4.5.2.jar:/Users/marcusboehm/.sbt/boot/scala-2.12.6/lib/scala-library.jar -H:Class=example.Hello -H:+JNI -H:IncludeResources=libhello.dylib -Djava.library.path=. -H:CLibraryPath=. -Djna.debug_load=true -Djna.library.path=/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jnalib -H:JNIConfigurationFiles=/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jniconfig
 echo ">>>>>>>>>>>> Running the binary"
 ./hello
