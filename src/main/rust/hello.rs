e7615e0e8b6f3f6b57aceb807d7b089a9cd588c1
Marcus Böhm <wif.mboehm@gmx.de>
2018-09-12 11:05:23 +0200

    cleanup

--- src/main/rust/hello.rs
+++ src/main/rust/hello.rs
@@ -5,16 +5,9 @@ use std::mem;
 use std::str;
 use std::os::raw::c_char;
 
-// fn main() {
-//     let s = hello();
-//     println!("{}", s)
-// }
-
 #[no_mangle]
 #[allow(non_snake_case)]
 pub extern fn printHello() {
-    // Convert the C string to a Rust one
-//    let name = to_string(name);
     println!("Hello! (This was printed in Rust)");
 }
 
@@ -22,7 +15,6 @@ pub extern fn printHello() {
 #[no_mangle]
 #[allow(non_snake_case)]
 pub fn hello() -> *const c_char {
-//    return "Hello from Rust!".to_string();
     return to_ptr("Hello from Rust!".to_string())
 }
 
@@ -32,12 +24,6 @@ pub fn formatHello(str: *const c_char) -> *const c_char {
     let argAsString = to_string(str);
     return to_ptr(format!("Hello {}, Rust is saying hello to you!", argAsString))
 }
-//
-//#[no_mangle]
-//#[allow(non_snake_case)]
-//pub fn otherHellowWorld() -> String {
-//    return "Hello from other Rust!".to_string();
-//}
 
 /// Convert a native string to a Rust string
 fn to_string(pointer: *const c_char) -> String {
