77166ef58d2a4b05d7abe22514abf63e059d1465
Marcus Böhm <wif.mboehm@gmx.de>
2018-09-12 17:38:07 +0200

    rename binary

--- run_native.sh
+++ run_native.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm example.hello
+rm hello
 echo ">>>>>>>>>>>> Compiling the rust sources"
 rustc --out-dir . src/main/rust/hello.rs
 echo ">>>>>>>>>>>> Generating the Header files"
@@ -7,7 +7,6 @@ cbindgen -o hello.h -l C src/main/rust/hello.rs
 echo ">>>>>>>>>>>> Compiling the scala sources"
 sbt package
 echo ">>>>>>>>>>>> Generating the native image"
-#native-image --verbose -cp target/scala-2.12/hello-scala_2.12-0.1.0-SNAPSHOT.jar:/Users/marcusboehm/.ivy2/cache/net.java.dev.jna/jna/jars/jna-4.5.2.jar:/Users/marcusboehm/.sbt/boot/scala-2.12.6/lib/scala-library.jar -H:Class=example.Hello -H:+JNI -H:IncludeResources=darwin/libhello.dylib,darwin/libjnidispatch.jnilib -Djava.library.path=. -H:CLibraryPath=.
-native-image --verbose -cp target/scala-2.12/hello-scala_2.12-0.1.0-SNAPSHOT.jar:/Users/marcusboehm/.ivy2/cache/net.java.dev.jna/jna/jars/jna-4.5.2.jar:/Users/marcusboehm/.sbt/boot/scala-2.12.6/lib/scala-library.jar -H:Class=example.Hello -H:+JNI -H:IncludeResources=libhello.dylib -Djava.library.path=. -H:CLibraryPath=. -Djna.debug_load=true -Djna.library.path=/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jnalib
+native-image -H:Name=hello --verbose -cp target/scala-2.12/hello-scala_2.12-0.1.0-SNAPSHOT.jar:/Users/marcusboehm/.ivy2/cache/net.java.dev.jna/jna/jars/jna-4.5.2.jar:/Users/marcusboehm/.sbt/boot/scala-2.12.6/lib/scala-library.jar -H:Class=example.Hello -H:+JNI -H:IncludeResources=libhello.dylib -Djava.library.path=. -H:CLibraryPath=. -Djna.debug_load=true -Djna.library.path=/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jnalib
 echo ">>>>>>>>>>>> Running the binary"
-./example.hello
+./hello
