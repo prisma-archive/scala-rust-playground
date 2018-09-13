53ef7554d9b7e15c429af58ae92b84749ef826cf
Marcus Böhm <wif.mboehm@gmx.de>
2018-09-13 10:07:33 +0200

    cleanup; comment JNA for now

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,14 +1,12 @@
 package example
 
-import com.sun.jna.{Function, Library, Native}
-import org.graalvm.nativeimage.c
 import org.graalvm.nativeimage.c.`type`.CTypeConversion
 
-object Hello extends Greeting {
-  System.setProperty("jna.debug_load", "true")
-  System.setProperty("jna.library.path", "/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/")
-  val library = Native.loadLibrary("hello", classOf[RustInterfaceJna])
-  library.printHello()
+object Hello {
+//  System.setProperty("jna.debug_load", "true")
+//  System.setProperty("jna.library.path", "/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/")
+//  val library = Native.loadLibrary("hello", classOf[RustInterfaceJna])
+//  library.printHello()
 //  classOf[Native].getMethod("invokeVoid", classOf[Function], classOf[Long], classOf[Integer], classOf[Array[Object]])
 //  classOf[Native].getDeclaredMethods.foreach { method =>
 //    classOf[Native].getDeclaredMethod(method.getName, method.getParameterTypes: _*)
@@ -16,21 +14,18 @@ object Hello extends Greeting {
 
   def main(args: Array[String]): Unit = {
     // debug flags for loading JNA itself
-    System.setProperty("jna.debug_load.jna", "true")
+//    System.setProperty("jna.debug_load.jna", "true")
 //    System.setProperty("jna.boot.library.path", "/Users/marcusboehm/R/github.com/graphcool/scala-rust-playground/jnalib/")
     // debug flags for loading libraries with JNA
-    testJna()
+//    testJna()
 
-//    testGraalsCApi()
+    testGraalsCApi()
   }
-
-  def testJna(): Unit = {
-//    val libc = Native.loadLibrary("c", classOf[libc])
-//    println(libc.puts("hello c"))
-
-//    val library = Native.loadLibrary("hello", classOf[RustInterfaceJna])
-    library.printHello()
-  }
+//
+//  def testJna(): Unit = {
+////    val library = Native.loadLibrary("hello", classOf[RustInterfaceJna])
+//    library.printHello()
+//  }
 
   def testGraalsCApi(): Unit = {
     RustInterfaceGraal.printHello()
@@ -58,15 +53,4 @@ object Hello extends Greeting {
     RustInterfaceGraal.increment(struct)
     println(s"count is: ${struct.getCount}")
   }
-}
-
-trait libc extends Library {
-  def open(path:String, flag:Int):Int
-  def ioctl(fd:Int, request:Int, args:Array[_]):Int
-  def close(fd:Int):Unit
-  def puts(s: String): Int
-}
-
-trait Greeting {
-  lazy val greeting: String = "hello alda!"
 }
